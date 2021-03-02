4d5a3cd04be3d17466144d867a9d1dc9038c4e07
unknown <danielfilipeschwingel@gmail.com>
2021-03-02 20:57:26 -0300

    Fifth class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
@@ -6,29 +7,53 @@ import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Início | move.it</title>
-			</Head>
-			<ExperienceBar/>
-			<CountdownProvider>
-				<section>
-					<div>
-						<Profile/>
-						<CompletedChallenges/>
-						<Countdown/>
-					</div>
-					<div>
-						<ChallengeBox/>
-					</div>
+interface HomeProps {
+	level: number;
+	currentExperience: number;
+	challengeCompleted: number;
+}
 
-				</section>
-			</CountdownProvider>
-		</div>
+export default function Home(props: HomeProps) {
+	return (
+		<ChallengesProvider 
+			level={props.level} 
+			currentExperience={props.currentExperience} 
+			challengeCompleted={props.challengeCompleted}
+		>
+			<div className={styles.container}>
+				<Head>
+					<title>Início | move.it</title>
+				</Head>
+				<ExperienceBar/>
+				<CountdownProvider>
+					<section>
+						<div>
+							<Profile/>
+							<CompletedChallenges/>
+							<Countdown/>
+						</div>
+						<div>
+							<ChallengeBox/>
+						</div>
+
+					</section>
+				</CountdownProvider>
+			</div>
+		</ChallengesProvider>
 	)
+}
+
+export const getServerSideProps:GetServerSideProps  = async (ctx)=> {
+	const { level, currentExperience, challengeCompleted } = ctx.req.cookies;
+	return {
+		props: {
+			level: Number(level),
+			currentExperience: Number(currentExperience),
+			challengeCompleted: Number(challengeCompleted)
+		}
+	}
 }
